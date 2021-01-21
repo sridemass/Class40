f9eb9cc1f6d006bc91df273422e741302a110d5c
sridemass <73376836+sridemass@users.noreply.github.com>
2021-01-21 15:50:46 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -13,12 +13,12 @@ var cars, car1, car2, car3, car4;
 var track, car1_img, car2_img, car3_img, car4_img;
 
 function preload(){
-  track = loadImage("images/track.jpg");
-  car1_img = loadImage("images/car1.png");
-  car2_img = loadImage("images/car2.png");
-  car3_img = loadImage("images/car3.png");
-  car4_img = loadImage("images/car4.png");
-  ground = loadImage("images/ground.png");
+  track = loadImage("C:\Users\Admin\Desktop\WhiteHatJr\carRacingStage2-master\images\track.jpg");
+  car1_img = loadImage("C:\Users\Admin\Desktop\WhiteHatJr\carRacingStage2-master\images\car1.png");
+  car2_img = loadImage("C:\Users\Admin\Desktop\WhiteHatJr\carRacingStage2-master\images\car2.png");
+  car3_img = loadImage("C:\Users\Admin\Desktop\WhiteHatJr\carRacingStage2-master\images\car3.png");
+  car4_img = loadImage("C:\Users\Admin\Desktop\WhiteHatJr\carRacingStage2-master\images\car4.png");
+  ground = loadImage("C:\Users\Admin\Desktop\WhiteHatJr\carRacingStage2-master\images\ground.png");
 }
 
 function setup(){
